09a08be15c46ce1f4e606ede33bf08fc9de11c05
Soanvig <soanvig@gmail.com>
2021-04-20 00:11:20 +0200

    Generate board on ensureGame

--- frontend/src/infrastructure/stateRepository.ts
+++ frontend/src/infrastructure/stateRepository.ts
@@ -1,4 +1,6 @@
+import { getBoard } from '../domain/Board';
 import { Game } from '../domain/Game';
+import { tokens } from '../domain/tokens';
 import { db, firestore } from './firebase';
 
 export const stateRepository = {
@@ -20,7 +22,14 @@ export const stateRepository = {
     if (!doc.exists) {
       await db.collection('games').doc(gameId).set({
         players: [],
-      });
+        board: getBoard(3).map(tile => ({
+          ...tile,
+          token: ({
+            ...tokens['base'],
+            direction: { x: 0, y: -1, z: 1 },
+          })
+        })),
+      } as Game);
     }
   },
 };
